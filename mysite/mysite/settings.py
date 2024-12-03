14b7f6b41b8119026346b82d3e75e1b6ef6f5f5c
Ariane Saulnier <asaulnie@made-f0Br3s7.clusters.42paris.fr>
2024-12-03 19:21:04 +0100

    allowed hosts added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p9g%@eo7tre^@3gqd+i%75*-*a=6neua70#6jk$m9s1f=zm$f#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
