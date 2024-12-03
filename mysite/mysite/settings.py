4169b440a062d394087454fbdeee19c314f8974d
Ariane Saulnier <asaulnie@made-f0Br3s7.clusters.42paris.fr>
2024-12-03 19:23:03 +0100

    added imports in seetings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_wsgi_application()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
